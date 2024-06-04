c7d92a68c65be969cb6f766c0fd6b190316c1a61
gagatpadi <padiyogaradhana@gmail.com>
2024-06-04 17:39:36 +0700

    jika terdapat file

--- StreamReaderandWriter/StreamReaderandWriter.cpp
+++ StreamReaderandWriter/StreamReaderandWriter.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 int main() {
-	string baring;
+	string baris;
 
 	//membuka file dalam mode menulis.
 	ofstream outfile;
@@ -33,5 +33,21 @@ int main() {
 	infile.open("contohfile.txt");
 
 	cout << endl << ">= Membuka dan membaca file" << endl;
+	// jika file ada maka
+	if (infile.is_open())
+	{
+		// melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			// dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		// tutup file tersebut setelah selesai
+		infile.close();
+	}
+	// jika tidak di temukan file maka akan menampilkan ini
+	else cout << "Unable to open this file";
+	return 0;
 
+	
 }
